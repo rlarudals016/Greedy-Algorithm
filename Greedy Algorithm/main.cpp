6789d1b904939d1627fa5b67e14ee0795628ab8f
KyeongminKim <rlarudals016@naver.com>
2018-07-30 16:17:14 +0900

    Initial Commit

---
+++ Greedy Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Greedy Algorithm
+//
+//  Created by MacUser on 2018. 7. 30..
+//  Copyright © 2018년 MacUser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
